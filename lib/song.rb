ffd2350596cb78cb79c78d12062a140c70df27d9
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-08-03 04:41:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,10 +14,6 @@ class Song
     @@songs << self
   end
 
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
-
   def self.all
     @@songs
   end
@@ -25,8 +21,4 @@ class Song
   def artist=(artist)
     @artist = artist
   end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
